8749c7bd06cf7db144651ad9edf1ec1af5346ee5
Denis Mamaev <expertykt@mail.ru>
2021-02-26 10:41:18 +0900

    Update ActionLink.js

--- src/components/ActionLink.js
+++ src/components/ActionLink.js
@@ -7,11 +7,10 @@ export default class ActionLink extends React.Component {
     render() {
         let action = _.get(this.props, 'action', null);
         return (
-            <Link to={withPrefix(_.get(action, 'url', null))}
+            <Link onClick={withPrefix(_.get(action, 'onClick', null))} 
+                   to={withPrefix(_.get(action, 'url', null))}
               {...(_.get(action, 'new_window', null) ? ({target: '_blank'}) : null)}
-             {...((_.get(action, 'new_window', null) || _.get(action, 'no_follow', null)) ? ({rel: (_.get(action, 'new_window', null) ? ('noopener ') : '') + (_.get(action, 'no_follow', null) ? ('nofollow') : '')}) : null)}>{_.get(action, 'label', null)}
-                onClick={withPrefix(_.get(action, 'onClick', null))}
-            </Link>
+             {...((_.get(action, 'new_window', null) || _.get(action, 'no_follow', null)) ? ({rel: (_.get(action, 'new_window', null) ? ('noopener ') : '') + (_.get(action, 'no_follow', null) ? ('nofollow') : '')}) : null)}>{_.get(action, 'label', null)}</Link>
         );
     }
 }
