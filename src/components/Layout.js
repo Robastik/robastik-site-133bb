52c8578f015aa10904806dbba5407e2e251bfe6c
Denis Mamaev <expertykt@mail.ru>
2021-10-19 11:11:38 +0900

    Layout.js
    let imgpath = _.get(this.props, 'imgpath', 'images/girl-and-boy-doing-discussion-green.png');

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -10,7 +10,7 @@ import Footer from './Footer';
 export default class Body extends React.Component {
     render() {
         let sitedomain = _.trim(_.get(this.props, 'pageContext.site.siteMetadata.domain', null), '/');
-        
+        let imgpath = _.get(this.props, 'imgpath', 'images/girl-and-boy-doing-discussion-green.png');
         let title = _.get(this.props, 'pageContext.frontmatter.title', null) + ' | ' + _.get(this.props, 'pageContext.site.siteMetadata.title', null);
         let font = _.get(this.props, 'pageContext.site.siteMetadata.base_font', null) || 'nunito-sans';
         if (_.get(this.props, 'pageContext.frontmatter.meta_title', null)) {
