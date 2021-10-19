b6415e0d995d25cbf0f096303ffa509974444320
Denis Mamaev <expertykt@mail.ru>
2021-10-19 11:03:04 +0900

    Layuot.js
    sitedomain + imgpath

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -21,7 +21,7 @@ export default class Body extends React.Component {
                 <Helmet>
                     <title>{title}</title>
                     <meta charSet="utf-8"/>
-                    <meta property="og:image" content={"https://" + sitedomain + this.props.imgpath}/>  
+                    <meta property="og:image" content={"https://" + sitedomain + imgpath}/>  
                     <meta name="viewport" content="width=device-width, initialScale=1.0" />
                     <meta name="google" content="notranslate" />
                     <meta name="description" content={_.get(this.props, 'pageContext.frontmatter.meta_description', null)}/>
