ba1b610ff68f94a9bbae496147cb83a285f4bc51
Denis Mamaev <expertykt@mail.ru>
2021-11-25 15:28:19 +0900

    Update post.js

--- src/templates/post.js
+++ src/templates/post.js
@@ -26,7 +26,7 @@ export default class Post extends React.Component {
           <Helmet>
           <meta property="og:image" content={"https://" + _.trim(_.get(this.props, 'pageContext.site.siteMetadata.domain', null), '/') + withPrefix(_.get(this.props, 'pageContext.frontmatter.image', null))}/>  
           <meta property="og:type" content="article"/>
-          <meta property="og:title" content={_.get(this.props, 'pageContext.frontmatter.subtitle', null) + " : " + _.get(this.props, 'pageContext.frontmatter.subtitle', null)}/>
+          <meta property="og:title" content={_.get(this.props, 'pageContext.frontmatter.title', null) + " : " + _.get(this.props, 'pageContext.frontmatter.subtitle', null)}/>
           <meta property="og:url" content={"https://" + _.trim(_.get(this.props, 'pageContext.site.siteMetadata.domain', null), '/') + withPrefix(_.get(this.props, 'pageContext.url', null))}/>
           <meta property="og:description" content={_.get(this.props, 'pageContext.frontmatter.meta_description', null)}/>
           </Helmet>
