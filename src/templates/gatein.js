9901f26633f1c935482221103a9b68d2e281748f
Denis Mamaev <expertykt@mail.ru>
2023-01-20 13:04:47 +0900

    GateIn test #3

--- src/templates/gatein.js
+++ src/templates/gatein.js
@@ -42,7 +42,7 @@ export default class Post extends React.Component {
                   </header>
                   <div className="post-content">
                   <iframe src="https://script.google.com/macros/s/AKfycbxNiXS-xyE1ULA4eYOlC_75RyvTkZIGh4H8nxd1Rv0-bTrq-30bN_YJ2_OoCVjX78xx/exec" 
-style="width: 100%; height: 400px; border: 0" background-color = "#f7f9fb"></iframe>
+style="width: 100%; height: 400px; border: 0"></iframe>
                   </div>
                   <BlogPostFooter {...this.props} page={this.props.pageContext} date_type={'long'} />
                 </article>
