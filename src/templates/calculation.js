7159d4583c5a7717cb81ad1113390679c395a846
Denis Mamaev <expertykt@mail.ru>
2023-01-24 13:29:59 +0900

    Calc #11

--- src/templates/calculation.js
+++ src/templates/calculation.js
@@ -28,7 +28,7 @@ export default class Post extends React.Component {
           <meta property="og:title" content={_.get(this.props, 'pageContext.frontmatter.title', null) + " : " + _.get(this.props, 'pageContext.frontmatter.subtitle', null)}/>
           <meta property="og:url" content={"https://" + _.trim(_.get(this.props, 'pageContext.site.siteMetadata.domain', null), '/') + withPrefix(_.get(this.props, 'pageContext.url', null))}/>
           <meta property="og:description" content={_.get(this.props, 'pageContext.frontmatter.meta_description', null)}/>
-          <script src={'/iframeSource.js'}/>
+          <script src={'iframeSource.js'} type="text/javascript" />
           </Helmet>
             <div className="outer">
               <div className="inner-medium">
