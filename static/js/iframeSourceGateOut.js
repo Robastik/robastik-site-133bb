8d5ba836be26b95bc957afaaf665cfb171243702
Denis Mamaev <expertykt@mail.ru>
2023-01-27 09:37:30 +0900

    GateIn

--- static/js/iframeSourceGateOut.js
+++ static/js/iframeSourceGateOut.js
@@ -1,7 +1,7 @@
 //Скрипт загружает iframe из источника, переданного на страницу в get параметрах
 window.onload = function(){
     let params = new URLSearchParams(window.location.search);
-    let idWebApp = 'AKfycbyuRk-1lhrO-kTn41MxhKNqezI9CNRx9ZP8LeKoMthr7shqntz_b1hAIVneeDRCuGSUvg';
+    let idWebApp = 'AKfycbyJ8z2n59e1AmaIbWF1jlrG-vYgCyrKOiA4418gXZSjlAagcQyd6f3e6JFQEVBsLFcy0Q';
     let urlWebApp = 'https://script.google.com/macros/s/'+ idWebApp +'/exec';
     document.getElementById("webapp").setAttribute("src", urlWebApp +'?'+ params.toString());
 }
