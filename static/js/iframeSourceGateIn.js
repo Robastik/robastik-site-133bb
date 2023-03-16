f2e8b4d9760aaa28526e9ecd713a8b1a17f7f3dc
Denis Mamaev <expertykt@mail.ru>
2023-03-16 15:47:31 +0900

    priceList

--- static/js/iframeSourceGateIn.js
+++ static/js/iframeSourceGateIn.js
@@ -2,7 +2,7 @@
 window.onload = function(){
     let params = new URLSearchParams(window.location.search);
     //Опубликованный WebApp Копир Калькуляции
-    let idWebApp = 'AKfycbwCH0LaPgo_cngpwAqAOcobeDGLwE4ZVaXSChbpVj477rTZWS17SoV0WaekaPLu51s2Wg';
+    let idWebApp = 'AKfycbxw6EY5oY5sCINnyJ7LjxJ2wnNXqWvINcaXYWEYO16aQW5-nCqXhJf6SWimwSDRxe4JDA';
     let urlWebApp = 'https://script.google.com/macros/s/'+ idWebApp +'/exec';
     document.getElementById("webapp").setAttribute("src", urlWebApp +'?'+ params.toString());
 }
