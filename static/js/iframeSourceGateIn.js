6b1637db9aefeb043b1e55e5e81551e3c79b736e
Denis Mamaev <expertykt@mail.ru>
2023-03-17 09:49:10 +0900

    priceList

--- static/js/iframeSourceGateIn.js
+++ static/js/iframeSourceGateIn.js
@@ -2,7 +2,16 @@
 window.onload = function(){
     let params = new URLSearchParams(window.location.search);
     //Опубликованный WebApp Копир Калькуляции
-    let idWebApp = 'AKfycbzqvfFysrVk6yTfLXqlMGWUxaMYbYkKG8mfSlHHf3Y97dbHo_71SsFe9hG_XdUqLRXfLA';
+    let idWebApp = 'AKfycbxeZvn0vZSy1MzmvlRtmsXIi2qD6D0uMc11NnY8Yh81NYf0ak4z24-BuOhJ1hNDl49yrg';
     let urlWebApp = 'https://script.google.com/macros/s/'+ idWebApp +'/exec';
+    document.getElementById("webapp").setAttribute("src", urlWebApp +'?'+ params.toString());
+    if(params.has("height")){//высота iframe
+        //для прайс-листа надо меньше высоты
+        document.getElementById("webapp").setAttribute("height", params.get("height"));
+    }else{
+        //по умолчанию и для генератора счетов высота 2000 норм
+        document.getElementById("webapp").setAttribute("height", 2000);
+    }
+    
     document.getElementById("webapp").setAttribute("src", urlWebApp +'?'+ params.toString());
 }
