332e3b58cfce83dda9120de54bfd73b2293b913a
Denis Mamaev <expertykt@mail.ru>
2023-03-17 10:44:17 +0900

    priceList

--- static/js/iframeSource.js
+++ static/js/iframeSource.js
@@ -1,8 +1,18 @@
 //Скрипт загружает iframe из источника, переданного на страницу в get параметрах
 window.onload = function(){
     let params = new URLSearchParams(window.location.search);
+
+    if(params.has("height")){//высота iframe
+        //для прайс-листа надо меньше высоты
+        document.getElementById("builder").setAttribute("height", '"'+ params.get("height") +'"');
+    }else{
+        //по умолчанию и для генератора счетов высота 2000 норм
+        document.getElementById("builder").setAttribute("height", "2000");
+    }
+
     let fileId = params.get('fileId'); // spreadsheet ID
     let sheetId = params.get('sheetId'); // gid → sheet ID
     let iframeSource = 'https://docs.google.com/spreadsheets/d/'+fileId+'/edit?usp=sharing&rm=minimal#gid='+sheetId;
     document.getElementById("builder").setAttribute("src", iframeSource);
+
 }
--- static/js/iframeSourceGateIn.js
+++ static/js/iframeSourceGateIn.js
@@ -2,14 +2,6 @@
 window.onload = function(){
     let params = new URLSearchParams(window.location.search);
 
-    if(params.has("height")){//высота iframe
-        //для прайс-листа надо меньше высоты
-        document.getElementById("webapp").setAttribute("height", '"'+ params.get("height") +'"');
-    }else{
-        //по умолчанию и для генератора счетов высота 2000 норм
-        document.getElementById("webapp").setAttribute("height", "2000");
-    }
-
     //Опубликованный WebApp Копир Калькуляции
     let idWebApp = 'AKfycbxeZvn0vZSy1MzmvlRtmsXIi2qD6D0uMc11NnY8Yh81NYf0ak4z24-BuOhJ1hNDl49yrg';
     let urlWebApp = 'https://script.google.com/macros/s/'+ idWebApp +'/exec';
