6f87ebc0838d69e1e60fcde0a4ab8609c1e9a7e4
Denis Mamaev <expertykt@mail.ru>
2023-03-21 10:19:15 +0900

    copier

--- static/js/iframeSourceGateIn.js
+++ static/js/iframeSourceGateIn.js
@@ -3,7 +3,7 @@ window.onload = function(){
     let params = new URLSearchParams(window.location.search);
 
     //Опубликованный WebApp Копир Калькуляции
-    let idWebApp = 'AKfycbzUo511YgVn9tkJwBLnB6dR-B0vuK9Dk0XeBP4q23HN5ApoqahtJs8s8c--wps4pBerfw';
+    let idWebApp = 'AKfycbwpzTWMPdSaKBswBmH53kuKfLY-_LCzkp4Z-uKv_DXFuogA5qEfKgSl0yMW8F2FQNyLTQ';
     let urlWebApp = 'https://script.google.com/macros/s/'+ idWebApp +'/exec';
     document.getElementById("webapp").setAttribute("src", urlWebApp +'?'+ params.toString());
 }
