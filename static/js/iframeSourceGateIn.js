e7ae6c65e9c7d03a9ab5abd91c2949ed02285319
Denis Mamaev <expertykt@mail.ru>
2023-03-21 12:46:23 +0900

    bfcache

--- static/js/iframeSourceGateIn.js
+++ static/js/iframeSourceGateIn.js
@@ -5,12 +5,14 @@ window.onload = function(){
     //Опубликованный WebApp Копир Калькуляции
     let idWebApp = 'AKfycbwpzTWMPdSaKBswBmH53kuKfLY-_LCzkp4Z-uKv_DXFuogA5qEfKgSl0yMW8F2FQNyLTQ';
     let urlWebApp = 'https://script.google.com/macros/s/'+ idWebApp +'/exec';
+    document.getElementById("webapp").setAttribute("src", "https://firebasestorage.googleapis.com/v0/b/robastik-2cb02.appspot.com/o/page%2Fpreloader.html?alt=media&token=be7ee1fb-8cca-493c-a552-8ea45a3b3e4a");
     document.getElementById("webapp").setAttribute("src", urlWebApp +'?'+ params.toString());
+
+    window.addEventListener ('pageshow', function (event) {
+        if (event.persisted) {
+          console.log ('Эта страница восстановлена ​​из BFcache.');
+        } else {
+          console.log ('Эта страница загрузилась по запросу.');
+        }
+    });
 }
-window.addEventListener ('pageshow', function (event) {
-    if (event.persisted) {
-      console.log ('Эта страница восстановлена ​​из BFcache.');
-    } else {
-      console.log ('Эта страница загрузилась по запросу.');
-    }
-});
