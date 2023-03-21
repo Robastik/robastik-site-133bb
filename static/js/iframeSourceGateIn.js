4600eaf74517b621747639bb95667f727a4645eb
Denis Mamaev <expertykt@mail.ru>
2023-03-21 13:07:16 +0900

    bfcache

--- static/js/iframeSourceGateIn.js
+++ static/js/iframeSourceGateIn.js
@@ -8,13 +8,21 @@ window.onload = function(){
     document.getElementById("webapp").setAttribute("src", "https://firebasestorage.googleapis.com/v0/b/robastik-2cb02.appspot.com/o/page%2Fpreloader.html?alt=media&token=be7ee1fb-8cca-493c-a552-8ea45a3b3e4a");
     document.getElementById("webapp").setAttribute("src", urlWebApp +'?'+ params.toString());
 
-    window.addEventListener ('pageshow', function (event) {
-        console.log ('pageshow');
-        const navigationType = performance.getEntriesByType('navigation')[0].type;
-        if (event.persisted || navigationType == 'back_forward' ) {
-          console.log ('Эта страница восстановлена ​​из BFcache.');
-        } else {
-          console.log ('Эта страница загрузилась по запросу.');
-        }
-    });
+    const events = ["pagehide", "pageshow", "unload", "load"];
+
+const eventLogger = (event) => {
+  switch (event.type) {
+    case "pagehide":
+    case "pageshow": {
+      let isPersisted = event.persisted ? "persisted" : "not persisted";
+      console.log(`Event: ${event.type} - ${isPersisted}`);
+      break;
+    }
+    default:
+      console.log(`Event: ${event.type}`);
+      break;
+  }
+};
+
+events.forEach((eventName) => window.addEventListener(eventName, eventLogger));
 }
