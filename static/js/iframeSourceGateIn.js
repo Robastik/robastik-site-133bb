6eb7efe30024b70609c6a7923cfd7604d2dd3455
Denis Mamaev <expertykt@mail.ru>
2023-03-21 16:20:39 +0900

    bgimage

--- static/js/iframeSourceGateIn.js
+++ static/js/iframeSourceGateIn.js
@@ -1,6 +1,6 @@
 //Скрипт загружает iframe из источника, переданного на страницу в get параметрах
 window.onload = function(){
-    document.getElementsByClassName("post-content").setAttribute("style", "background-image: url('https://firebasestorage.googleapis.com/v0/b/robastik-2cb02.appspot.com/o/Preloader_webapp.gif?alt=media&token=e6306d2f-4e41-4fd8-aabc-066f0a097095'); height: 15%; width: 15%;");
+    document.getElementsByClassName("post-content").style.backgroundImage("url('https://firebasestorage.googleapis.com/v0/b/robastik-2cb02.appspot.com/o/Preloader_webapp.gif?alt=media&token=e6306d2f-4e41-4fd8-aabc-066f0a097095')");
 
     let params = new URLSearchParams(window.location.search);
 
