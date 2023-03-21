acf802aae94d996f08a947b4623341c76e440944
Denis Mamaev <expertykt@mail.ru>
2023-03-22 08:53:18 +0900

    bgposition

--- static/js/iframeSourceGateIn.js
+++ static/js/iframeSourceGateIn.js
@@ -3,7 +3,7 @@ window.onload = function(){
     let divBackground = document.getElementsByClassName("post-content")[0];
     divBackground.style.backgroundImage = "url('https://firebasestorage.googleapis.com/v0/b/robastik-2cb02.appspot.com/o/Preloader_webapp.gif?alt=media&token=e6306d2f-4e41-4fd8-aabc-066f0a097095')";
     divBackground.style.backgroundRepeat = "no-repeat";
-    divBackground.style.backgroundPosition = "20% 50%";
+    divBackground.style.backgroundPosition = "50% 20%";
 
     let params = new URLSearchParams(window.location.search);
 
