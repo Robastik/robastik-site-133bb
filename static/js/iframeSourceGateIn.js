1c14f2a70aae5b150aa8ba4b63e52b1a2d447249
Denis Mamaev <expertykt@mail.ru>
2023-03-21 14:42:23 +0900

    bfcache

--- static/js/iframeSourceGateIn.js
+++ static/js/iframeSourceGateIn.js
@@ -8,21 +8,11 @@ window.onload = function(){
     document.getElementById("webapp").setAttribute("src", urlWebApp +'?'+ params.toString());
 }
     
-var events = ["pageshow"];
-var eventLogger = (event) => {
-  switch (event.type) {
-    case "pageshow": {
+(function () {
+    window.onpageshow = function(event) {
         if (event.persisted) {
-            let params = new URLSearchParams(window.location.search);
-
-            //Опубликованный WebApp Копир Калькуляции
-            let idWebApp = 'AKfycbwpzTWMPdSaKBswBmH53kuKfLY-_LCzkp4Z-uKv_DXFuogA5qEfKgSl0yMW8F2FQNyLTQ';
-            let urlWebApp = 'https://script.google.com/macros/s/'+ idWebApp +'/exec';
-            document.getElementById("webapp").setAttribute("src", urlWebApp +'?'+ params.toString());
+            window.location.reload();
         }
-    break;
-    }
-  }
-};
-events.forEach((eventName) => window.addEventListener(eventName, eventLogger));
+    };
+})();
 
