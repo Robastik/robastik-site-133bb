f76a65cdc1d7dc206c5646b46da46f3fcebf4e77
Denis Mamaev <expertykt@mail.ru>
2023-03-23 10:22:58 +0900

    desktop

--- static/js/iframeSource.js
+++ static/js/iframeSource.js
@@ -4,6 +4,7 @@ window.onload = function(){
     if((/mobi/).test(window.navigator.userAgent.toLowerCase())){
         const iframeSource = "https://firebasestorage.googleapis.com/v0/b/robastik-2cb02.appspot.com/o/page%2FturnMobileOption.html?alt=media&token=51a82c94-0670-42db-9ebf-f8efb67ebc4e";
         document.getElementById("builder").setAttribute("src", iframeSource);
+        document.getElementById("builder").setAttribute("height", "2000");
     }else{//для десктопа показать google sheet калькуляцию
         let params = new URLSearchParams(window.location.search);
 
