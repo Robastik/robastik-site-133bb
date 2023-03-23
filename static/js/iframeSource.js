8bb65d322a6804cafb744446e1d50a28b2349bc9
Denis Mamaev <expertykt@mail.ru>
2023-03-23 10:11:42 +0900

    desktop

--- static/js/iframeSource.js
+++ static/js/iframeSource.js
@@ -1,18 +1,23 @@
 //Скрипт загружает iframe из источника, переданного на страницу в get параметрах
 window.onload = function(){
-    let params = new URLSearchParams(window.location.search);
+    //Для мобильных показывать инструкцию для перехода на Desktop site
+    if((/mobi/).test(window.navigator.userAgent.toLowerCase())){
+        const iframeSource = "https://firebasestorage.googleapis.com/v0/b/robastik-2cb02.appspot.com/o/page%2FturnMobileOption.html?alt=media&token=51a82c94-0670-42db-9ebf-f8efb67ebc4e";
+        document.getElementById("builder").setAttribute("src", iframeSource);
+    }else{//для десктопа показать google sheet калькуляцию
+        let params = new URLSearchParams(window.location.search);
 
-    if(params.has("height")){//высота iframe
-        //для прайс-листа надо меньше высоты
-        document.getElementById("builder").setAttribute("height", params.get("height"));
-    }else{
-        //по умолчанию и для генератора счетов высота 2000 норм
-        document.getElementById("builder").setAttribute("height", "2000");
+        if(params.has("height")){//высота iframe
+            //для прайс-листа надо меньше высоты
+            document.getElementById("builder").setAttribute("height", params.get("height"));
+        }else{
+            //по умолчанию и для генератора счетов высота 2000 норм
+            document.getElementById("builder").setAttribute("height", "2000");
+        }
+
+        let fileId = params.get('fileId'); // spreadsheet ID
+        let sheetId = params.get('sheetId'); // gid → sheet ID
+        let iframeSource = 'https://docs.google.com/spreadsheets/d/'+fileId+'/edit?usp=sharing&rm=minimal#gid='+sheetId;
+        document.getElementById("builder").setAttribute("src", iframeSource);
     }
-
-    let fileId = params.get('fileId'); // spreadsheet ID
-    let sheetId = params.get('sheetId'); // gid → sheet ID
-    let iframeSource = 'https://docs.google.com/spreadsheets/d/'+fileId+'/edit?usp=sharing&rm=minimal#gid='+sheetId;
-    document.getElementById("builder").setAttribute("src", iframeSource);
-
 }
