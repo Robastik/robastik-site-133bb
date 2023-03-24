5ee70ec43aec14527733881ecffef34a06f8592d
Denis Mamaev <expertykt@mail.ru>
2023-03-24 16:08:29 +0900

    backgroundSize

--- static/js/iframeSourceGateIn.js
+++ static/js/iframeSourceGateIn.js
@@ -8,9 +8,7 @@ window.onload = function(){
     divBackground.style.backgroundImage = "url('https://firebasestorage.googleapis.com/v0/b/robastik-2cb02.appspot.com/o/Preloader_webapp.gif?alt=media&token=e6306d2f-4e41-4fd8-aabc-066f0a097095')";
     divBackground.style.backgroundRepeat = "no-repeat";
     divBackground.style.backgroundPosition = "50% 0%";
-    divBackground.style.width = "3%";
-    divBackground.style.height = "3%";
-
+    divBackground.style.backgroundSize  = "3% 3%";
 
 
     let params = new URLSearchParams(window.location.search);
