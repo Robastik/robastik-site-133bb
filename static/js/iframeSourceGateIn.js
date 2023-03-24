8b6c042dde0162418738d9e3b8e5bbbc1a1e5827
Denis Mamaev <expertykt@mail.ru>
2023-03-24 16:29:42 +0900

    preload

--- static/js/iframeSourceGateIn.js
+++ static/js/iframeSourceGateIn.js
@@ -8,7 +8,7 @@ window.onload = function(){
     divBackground.style.backgroundImage = "url('https://firebasestorage.googleapis.com/v0/b/robastik-2cb02.appspot.com/o/page%2FPreloader_webapp.gif?alt=media&token=7a9b01d9-99a1-4ebe-9276-2d37bf9d4941')";
     divBackground.style.backgroundRepeat = "no-repeat";
     divBackground.style.backgroundPosition = "50% 0%";
-    divBackground.style.backgroundSize  = "5% 5%";
+    //divBackground.style.backgroundSize  = "5% 5%";
 
 
     let params = new URLSearchParams(window.location.search);
