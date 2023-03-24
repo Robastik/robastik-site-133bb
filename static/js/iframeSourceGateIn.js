fb293eeb3949999e68dc7b63c8ef3e0b09983299
Denis Mamaev <expertykt@mail.ru>
2023-03-24 15:24:26 +0900

    intro

--- static/js/iframeSourceGateIn.js
+++ static/js/iframeSourceGateIn.js
@@ -4,7 +4,7 @@
 
 //Скрипт загружает iframe из источника, переданного на страницу в get параметрах
 window.onload = function(){
-    let divBackground = document.getElementsByClassName("post-content")[0];
+    let divBackground = document.getElementsByClassName("webapp")[0];
     divBackground.style.backgroundImage = "url('https://firebasestorage.googleapis.com/v0/b/robastik-2cb02.appspot.com/o/Preloader_webapp.gif?alt=media&token=e6306d2f-4e41-4fd8-aabc-066f0a097095')";
     divBackground.style.backgroundRepeat = "no-repeat";
     divBackground.style.backgroundPosition = "50% 0%";
