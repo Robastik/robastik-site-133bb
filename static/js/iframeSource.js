d11340c5d85954c27a33d16033be175b027e9604
Denis Mamaev <expertykt@mail.ru>
2023-03-27 16:24:04 +0900

    denied

--- static/js/iframeSource.js
+++ static/js/iframeSource.js
@@ -6,6 +6,13 @@ window.onload = function(){
         document.getElementById("builder").setAttribute("src", iframeSource);
         document.getElementById("builder").setAttribute("height", "1500");
     }else{//для десктопа показать google sheet калькуляцию
+        //анимация прелоадера
+        let divBackground = document.getElementsByClassName("iframeHolder")[0];
+        divBackground.style.backgroundImage = "url('https://firebasestorage.googleapis.com/v0/b/robastik-2cb02.appspot.com/o/page%2FPreloader_webapp.gif?alt=media&token=7a9b01d9-99a1-4ebe-9276-2d37bf9d4941')";
+        divBackground.style.backgroundRepeat = "no-repeat";
+        divBackground.style.backgroundPosition = "50% 0%";
+
+
         let params = new URLSearchParams(window.location.search);
 
         if(params.has("height")){//высота iframe
@@ -15,7 +22,7 @@ window.onload = function(){
             //по умолчанию и для генератора счетов высота 2000 норм
             document.getElementById("builder").setAttribute("height", "2000");
         }
-
+        alert("Прайс-лист");
         let fileId = params.get('fileId'); // spreadsheet ID
         let sheetId = params.get('sheetId'); // gid → sheet ID
         let iframeSource = 'https://docs.google.com/spreadsheets/d/'+fileId+'/edit?usp=sharing&rm=minimal#gid='+sheetId;
