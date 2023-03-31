fbfd97f3ac4e56c5b7f836cd816994f42f567dcd
Denis Mamaev <expertykt@mail.ru>
2023-03-31 09:44:01 +0900

    licenseLinkElement

--- static/js/iframeSourceGateIn.js
+++ static/js/iframeSourceGateIn.js
@@ -15,24 +15,27 @@ window.onload = function(){
 
     let params = new URLSearchParams(window.location.search);
 
-    //даем ссылку на демо или реальный шаблон договора
-    let demoLicenseLink = 'https://firebasestorage.googleapis.com/v0/b/robastik-2cb02.appspot.com/o/sampleLegalDocs%2F%D0%A8%D0%B0%D0%B1%D0%BB%D0%BE%D0%BD%20%D0%B4%D0%BE%D0%B3%D0%BE%D0%B2%D0%BE%D1%80%D0%B0%20%D0%94%D0%B5%D0%BC%D0%BE.pdf?alt=media&token=2fd7df81-fb80-4074-9812-67d4184ce708';
-    let realLicenseLink = 'https://firebasestorage.googleapis.com/v0/b/robastik-2cb02.appspot.com/o/sampleLegalDocs%2F%D0%A8%D0%B0%D0%B1%D0%BB%D0%BE%D0%BD%20%D0%B4%D0%BE%D0%B3%D0%BE%D0%B2%D0%BE%D1%80%D0%B0.pdf?alt=media&token=5a1487d2-2c53-43b2-ad4c-470087975484';
+    //ссылка без адреса
+    let licenseLinkElement = document.querySelector('a[href=""]');
+    //если нашлась ссылка без адреса, то это страница invoice-message, в противном случае это price-message и ничего делать не нужно
+    if(licenseLinkElement){
+        //даем ссылку на демо или реальный шаблон договора
+        let demoLicenseLink = 'https://firebasestorage.googleapis.com/v0/b/robastik-2cb02.appspot.com/o/sampleLegalDocs%2F%D0%A8%D0%B0%D0%B1%D0%BB%D0%BE%D0%BD%20%D0%B4%D0%BE%D0%B3%D0%BE%D0%B2%D0%BE%D1%80%D0%B0%20%D0%94%D0%B5%D0%BC%D0%BE.pdf?alt=media&token=2fd7df81-fb80-4074-9812-67d4184ce708';
+        let realLicenseLink = 'https://firebasestorage.googleapis.com/v0/b/robastik-2cb02.appspot.com/o/sampleLegalDocs%2F%D0%A8%D0%B0%D0%B1%D0%BB%D0%BE%D0%BD%20%D0%B4%D0%BE%D0%B3%D0%BE%D0%B2%D0%BE%D1%80%D0%B0.pdf?alt=media&token=5a1487d2-2c53-43b2-ad4c-470087975484';
 
-    if(params.has("rm")){
-        let realMode = params.get('rm');
-        if(realMode){
-            var licenseLink = realLicenseLink
+        if(params.has("rm")){
+            let realMode = params.get('rm');
+            if(realMode){
+                var licenseLink = realLicenseLink
+            }else{
+                var licenseLink = demoLicenseLink
+            }
         }else{
             var licenseLink = demoLicenseLink
         }
-    }else{
-        var licenseLink = demoLicenseLink
-    }
 
-    //ссылка без адреса
-    let licenseLinkElement = document.querySelector('a[href=""]');
-    licenseLinkElement.href = licenseLink;
+        licenseLinkElement.href = licenseLink;
+    }
 
     //Опубликованный WebApp Копир Калькуляции
     //!!! И в следующей безымянной функции
