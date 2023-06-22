0b8965d54417f3bf9eebd10b7ea88563ff928bcb
Denis Mamaev <expertykt@mail.ru>
2023-06-22 15:46:08 +0900

    version

--- src/components/SectionMessage.js
+++ src/components/SectionMessage.js
@@ -11,27 +11,22 @@ export default class SectionMessage extends React.Component {
             <section id={_.get(section, 'section_id', null)} className={'block text-block bg-' + _.get(section, 'background', null) + ' outer'}>
               <div className="inner">
                 <div className="grid">
-                  {_.get(section, 'image', null) && (
-                  <div className="cell block-preview">
-                    <img src={withPrefix(_.get(section, 'image', null))} alt={_.get(section, 'image_alt', null)} />
-                  </div>
-                  )}
-                  <div className="cell block-content">
+                  <header className="post-header">
                     {_.get(section, 'title', null) && (
                     <h1 className="post-title">{_.get(section, 'title', null)}</h1>
                     )}
                     {_.get(section, 'subtitle', null) && (
                     <div className="post-subtitle">{_.get(section, 'subtitle', null)}</div>
                     )}
-                    <div className="post-content">
-                      {markdownify(_.get(section, 'content', null))}
-                    </div>
-                    {_.get(section, 'actions', null) && (
-                    <div className="block-buttons">
-                      <CtaButtons {...this.props} actions={_.get(section, 'actions', null)} />
-                    </div>
-                    )}
+                  </header>
+                  <div className="post-content">
+                    {markdownify(_.get(section, 'content', null))}
                   </div>
+                  {_.get(section, 'actions', null) && (
+                  <div className="block-buttons">
+                    <CtaButtons {...this.props} actions={_.get(section, 'actions', null)} />
+                  </div>
+                  )}
                 </div>
               </div>
             </section>
