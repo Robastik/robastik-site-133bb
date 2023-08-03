24ec5ca2636108ff2c0e0c11ddafaafc40a3746b
Denis Mamaev <expertykt@mail.ru>
2023-08-04 04:16:06 +0900

    video6

--- src/templates/video.js
+++ src/templates/video.js
@@ -48,9 +48,8 @@ export default class Post extends React.Component {
                   )}
                   <div className="post-content">
                     {markdownify(_.get(this.props, 'pageContext.frontmatter.part1', null))}
-                    {htmlToReact(_.get(this.props, 'pageContext.html', null))}
                     <iframe width="560" height="315" src="https://www.youtube.com/embed/VjiWpGR82t0?start=20" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                  
+                    {htmlToReact(_.get(this.props, 'pageContext.html', null))}
                   </div>
                   <BlogPostFooter {...this.props} page={this.props.pageContext} date_type={'long'} />
                 </article>
