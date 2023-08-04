4014792aa66dbe18033081cee3d9f82ace3d3b16
Denis Mamaev <expertykt@mail.ru>
2023-08-04 15:24:31 +0900

    video8

--- src/templates/video.js
+++ src/templates/video.js
@@ -48,7 +48,7 @@ export default class Post extends React.Component {
                   )}
                   <div className="post-content">
                     {markdownify(_.get(this.props, 'pageContext.frontmatter.part1', null))}
-                    <iframe width="560" height="315" src="https://www.youtube.com/embed/wAaUBo_rXaQ?start=16" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                    <iframe width="560" height="315" src={_.get(this.props, 'pageContext.frontmatter.youtube', null)} title="Как заполнить документ данными из Excel" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
                     {htmlToReact(_.get(this.props, 'pageContext.html', null))}
                   </div>
                   <BlogPostFooter {...this.props} page={this.props.pageContext} date_type={'long'} />
