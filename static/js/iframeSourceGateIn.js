c4873580ca65c16c22b37634b3e3332e9f42cd76
Denis Mamaev <expertykt@mail.ru>
2023-09-12 16:15:05 +0900

    css

--- static/js/iframeSourceGateIn.js
+++ static/js/iframeSourceGateIn.js
@@ -7,10 +7,10 @@ window.onload = function(){
     
     //анимация прелоадера
     //или в css, inline не собирается
-    let divBackground = document.getElementsByClassName("iframeHolder")[0];
-    divBackground.style.backgroundImage = "url('https://firebasestorage.googleapis.com/v0/b/robastik-2cb02.appspot.com/o/page%2FPreloader_webapp.gif?alt=media&token=7a9b01d9-99a1-4ebe-9276-2d37bf9d4941')";
-    divBackground.style.backgroundRepeat = "no-repeat";
-    divBackground.style.backgroundPosition = "50% 0%";
+    //let divBackground = document.getElementsByClassName("iframeHolder")[0];
+    //divBackground.style.backgroundImage = "url('https://firebasestorage.googleapis.com/v0/b/robastik-2cb02.appspot.com/o/page%2FPreloader_webapp.gif?alt=media&token=7a9b01d9-99a1-4ebe-9276-2d37bf9d4941')";
+    //divBackground.style.backgroundRepeat = "no-repeat";
+   // divBackground.style.backgroundPosition = "50% 0%";
 
 
     let params = new URLSearchParams(window.location.search);
